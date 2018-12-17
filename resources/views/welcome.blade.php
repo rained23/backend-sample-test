8efba2a562946bb017dd6ec6dbc28168808eefc3
Rain Naz <rained23@gmail.com>
2018-12-17 08:31:59 +0800

    using env var for name on web ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{ env('app.name', 'SocarTest')}}
+                    {{ config('app.name', 'SocarTest')}}
                 </div>
 
                
